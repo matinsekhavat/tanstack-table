970e406a623e7f5fb3dd00517506d7ff4442913a
matinsekhavat <matinhyphen@gmail.com>
2025-01-24 19:27:44 +0330

    feat: add mock data,columns we need for tanstack

---
+++ src/services/data.ts
@@ -0,0 +1,119 @@
+import { ColumnDef } from "@tanstack/react-table";
+
+export type User = {
+  firstName: string;
+  lastName: string;
+  age: number;
+  visits: number;
+  progress: number;
+  status: string;
+};
+export const mockUser: User[] = [
+  {
+    firstName: "Tanner",
+    lastName: "Linsley",
+    age: 33,
+    visits: 100,
+    progress: 50,
+    status: "Married",
+  },
+  {
+    firstName: "Kevin",
+    lastName: "Vandy",
+    age: 27,
+    visits: 200,
+    progress: 100,
+    status: "Single",
+  },
+  {
+    firstName: "Alice",
+    lastName: "Smith",
+    age: 30,
+    visits: 150,
+    progress: 80,
+    status: "Single",
+  },
+  {
+    firstName: "Bob",
+    lastName: "Johnson",
+    age: 25,
+    visits: 200,
+    progress: 90,
+    status: "Married",
+  },
+  {
+    firstName: "Charlie",
+    lastName: "Brown",
+    age: 28,
+    visits: 100,
+    progress: 70,
+    status: "In a relationship",
+  },
+  {
+    firstName: "Diana",
+    lastName: "Prince",
+    age: 35,
+    visits: 300,
+    progress: 60,
+    status: "Single",
+  },
+  {
+    firstName: "Ethan",
+    lastName: "Hunt",
+    age: 40,
+    visits: 250,
+    progress: 85,
+    status: "Married",
+  },
+  {
+    firstName: "Fiona",
+    lastName: "Glenanne",
+    age: 32,
+    visits: 180,
+    progress: 75,
+    status: "Single",
+  },
+  {
+    firstName: "George",
+    lastName: "Mason",
+    age: 29,
+    visits: 220,
+    progress: 95,
+    status: "In a relationship",
+  },
+  {
+    firstName: "Hannah",
+    lastName: "Montana",
+    age: 22,
+    visits: 120,
+    progress: 40,
+    status: "Single",
+  },
+];
+
+export const tableCols: ColumnDef<User>[] = [
+  {
+    accessorKey: "firstName", // Add this line
+    header: "First Name",
+  },
+  {
+    accessorKey: "lastName", // Add this line
+    header: "Last Name",
+  },
+  {
+    accessorKey: "age",
+    header: "Age",
+  },
+  {
+    accessorKey: "visits",
+    header: "Visits",
+  },
+  {
+    accessorKey: "progress",
+    header: "Progress",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+];
